56ae0b0cfa9b3ef2869491aba25e378ea651d7df
giangnhi07 <giangnhi0703@gmail.com>
2020-11-27 21:10:33 +0700

    create stack navigation

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import RootStack from './RootStack';
-
-export default function App() {
+function HomeScreen({navigation}) {
 	return (
-		<View style={styles.container}>
-			<RootStack />
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => {
+					navigation.navigate('Details');
+				}}
+			/>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+function DetailsScreen({navigation}) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Details Screen</Text>
+			<Button
+				title="Go to Details again"
+				onPress={() => {
+					navigation.push('Details');
+				}}
+			/>
+			<Button
+				title="Go to Home"
+				onPress={() => {
+					navigation.navigate('Home');
+				}}
+			/>
+			<Button
+				title="Go back"
+				onPress={() => {
+					navigation.goBack();
+				}}
+			/>
+		</View>
+	);
+}
+const Stack = createStackNavigator();
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={
+				{
+					headerStyle: {
+					  backgroundColor: '#f4511e',
+					},
+					headerTintColor: '#fff',
+					headerTitleStyle: {
+					  fontWeight: 'bold',
+					},
+				  }
+			}>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+export default App;
--- components/SwiperComponent.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export default function SwiperComponent() {
-	return <Text>SwiperComponent</Text>;
-}
